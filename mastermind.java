508149aed937436e2df34c322f74f01f73892613
remiSoulier <99562240+remiSoulier@users.noreply.github.com>
2022-11-03 13:41:58 +0100

    Update mastermind.java

--- mastermind.java
+++ mastermind.java
@@ -2,10 +2,14 @@ import java.util.*;
 
 public class mastermind {
     public static void main(String[] args) {
+        char[] couleurs = saisieCouleur(); 
+    }
+
+    public static char[] saisieCouleur(){
         Scanner input = new Scanner(System.in);
         System.out.println("saisir le nombre de couleurs possibles");
         int nbCoul = input.nextInt();
-        char[] tabCoul = new char[nbCoul-1];
+        char[] tabCoul = new char[nbCoul];
         for (int i =0 ; i< (tabCoul.length);i++){
             String coul = input.next();
             tabCoul[i]= coul.charAt(0);
@@ -13,5 +17,7 @@ public class mastermind {
         for (int i =0 ; i< (tabCoul.length);i++){
             System.out.print(tabCoul[i]+" ");
         }
+        input.close();
+        return tabCoul;
     }
 }

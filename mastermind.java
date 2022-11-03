7516b8cfd20e6d580e5b4d76819052427ad2fd82
remiSoulier <99562240+remiSoulier@users.noreply.github.com>
2022-11-03 13:36:34 +0100

    Add files via upload

---
+++ mastermind.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("saisir le nombre de couleurs possibles");
+        int nbCoul = input.nextInt();
+        char[] tabCoul = new char[nbCoul-1];
+        for (int i =0 ; i< (tabCoul.length);i++){
+            String coul = input.next();
+            tabCoul[i]= coul.charAt(0);
+        }
+        for (int i =0 ; i< (tabCoul.length);i++){
+            System.out.print(tabCoul[i]+" ");
+        }
+    }
+}
